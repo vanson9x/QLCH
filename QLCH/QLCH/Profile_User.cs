ce6f70805c87dd5d505b02dbef88fbd9cba3d065
Phạm Văn Sơn <Phm Vn Sn>
2016-07-23 16:12:48 +0700

    Design: hoàn thành giao diện thông tin tài khoản

---
+++ QLCH/QLCH/Profile_User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLCH
+{
+    public partial class Profile_User : Form
+    {
+        public Profile_User()
+        {
+            InitializeComponent();
+            btnSua.FlatStyle = FlatStyle.Flat;
+        }
+    }
+}
