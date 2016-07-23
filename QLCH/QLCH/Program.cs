6d3f266ed8442aa5c4303ab2c600112e8a36cfaa
Phạm Văn Sơn <Phm Vn Sn>
2016-07-23 12:29:24 +0700

    Design: hoàn thành màn hình chương trình chính

--- QLCH/QLCH/Program.cs
+++ QLCH/QLCH/Program.cs
@@ -15,7 +15,7 @@ namespace QLCH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new MainForm());
         }
     }
 }
